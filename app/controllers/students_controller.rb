ff42698375475d4ca1c5bc5cf79f31494f0df39b
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-02-28 14:51:37 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,16 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
 end
+
+
+# The user is able to go to /students/2 to
+# receive information on the student with an id of 2
+
+# The application should render the values
+# from the student record in the database
